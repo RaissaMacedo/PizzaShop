230ca261c5f7bd1e1629fadfa99f658c21d5ce29
RaissaMacedo <raissamacedo7@gmail.com>
2024-05-27 20:13:23 -0300

    created orders, page login and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 import './global.css'
 import { router } from './routes'
+
 import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
   return (
-   <HelmetProvider>
-    <Helmet titleTemplate="%s | pizza.shop"/>
-    <RouterProvider router={router}/>
-    </HelmetProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+      <HelmetProvider>
+        <Helmet titleTemplate="%s | pizza.shop"/>
+        <Toaster richColors />
+        <RouterProvider router={router}/>
+      </HelmetProvider>
+    </ThemeProvider>
   )
 }
 
